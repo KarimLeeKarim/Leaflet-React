11e54e70860e21f87e5c6f8daab483d5c2ce642e
KarimleeKarim <kerimlikerim24@gmail.com>
2021-06-18 21:40:37 +0400

    Added three new Markers as a example

--- src/Components/MapMarker.js
+++ src/Components/MapMarker.js
@@ -1,23 +1,33 @@
 import React from 'react';
 import L from 'leaflet';
-import icons from "leaflet/dist/images/marker-icon.png";
-import iconShadow from "leaflet/dist/images/marker-shadow.png";
+import leafGreen from "leaflet/dist/images/leaf-green.png";
+import leafRed from "leaflet/dist/images/leaf-red.png";
+import leafOrange from "leaflet/dist/images/leaf-orange.png";
+import leafShadow from "leaflet/dist/images/leaf-shadow.png";
 
 const MapMarker = ({ value }) => {
-    let myIcon = L.icon({
-        iconUrl: icons,
-        shadowUrl: iconShadow,
-        iconSize: null,
-        shadowSize: null,
-        iconSize: [22, 37],
-        shadowSize: [40, 54],
-        iconAnchor: [5, 34],
-        shadowAnchor: [4, 62],
-        popupAnchor: [5, -34],
+    let severalIcons = L.Icon.extend({
+        options: {
+            shadowUrl: leafShadow,
+            iconSize: [38, 95],
+            shadowSize: [50, 64],
+            iconAnchor: [22, 94],
+            shadowAnchor: [4, 62],
+            popupAnchor: [-3, -76]
+        }
     });
 
-    value !== undefined && L.marker([40.4240, 49.8386], { icon: myIcon }).addTo(value)
-        .bindPopup('<b>Hello</b>, <br>hope all is right?');
+    let greenIcon = new severalIcons({ iconUrl: leafGreen }),
+        redIcon = new severalIcons({ iconUrl: leafRed }),
+        orangeIcon = new severalIcons({ iconUrl: leafOrange });
+
+    L.icon = function (options) {
+        return new L.Icon(options);
+    };
+
+    value !== undefined && L.marker([40.4241, 49.8388], { icon: greenIcon }).addTo(value).bindPopup("Sweet Home");
+    value !== undefined && L.marker([40.4256, 49.8423], { icon: redIcon }).addTo(value).bindPopup("<b>Azadliq Metro</b><br/>The Freedom Subway");
+    value !== undefined && L.marker([40.4225, 49.8423], { icon: orangeIcon }).addTo(value).bindPopup("Baku Tennis Academy");
 
     return (
         <>
